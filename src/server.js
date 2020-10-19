f0ff406fbc5fa0c6dc89c31446a434fc3834229c
Gustavo Padilha <padilhagustavo97@gmail.com>
2020-10-19 00:28:38 -0300

    Next Level Week #3

---
+++ src/server.js
@@ -0,0 +1,32 @@
+// NPM - Node Pack Manager
+// cd ../ <- Subir um diretório
+// ls <- Mostrar diretório
+// json <- javascript object noteition
+// hbs <- handlebarsjs
+
+// Importar dependência
+const express = require("express");
+const path = require("path");
+const pages = require("./pages.js");
+
+// Iniciando o express
+const server = express();
+server
+  //  Utilizar body do req
+  .use(express.urlencoded({ extended: true }))
+  //Utilizando os arquivos estáticos
+  .use(express.static("public"))
+
+  // Configurar template engine
+  .set("views", path.join(__dirname, "views"))
+  .set("view engine", "hbs")
+
+  // Rotas da aplicação
+  .get("/", pages.index)
+  .get("/orphanage", pages.orphanage)
+  .get("/orphanages", pages.orphanages)
+  .get("/create-orphanage", pages.createOrphanage)
+  .post("/save-orphanage", pages.saveOrphanage);
+
+// ligar o servidor
+server.listen(5500);
